f9281b1ca07f529a0ab2a9735a943055b85e39a4
chen <1220404947@qq.com>
2019-01-23 20:29:59 +0800

    测试

---
+++ src/main/java/com/chen/test/Hello.java
@@ -0,0 +1,15 @@
+package com.chen.test;
+
+/**
+ * @author cy
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("赵大傻");
+        System.out.println("何二傻");
+        System.out.println("李三傻");
+        System.out.println("罗四傻");
+    }
+
+}
